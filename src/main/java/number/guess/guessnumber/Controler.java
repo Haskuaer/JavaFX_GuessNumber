7205f5fbae8b60840f1896029f723933a16af5f9
Haskuaer <101459312+Haskuaer@users.noreply.github.com>
2024-06-22 17:00:50 +0200

    Guessing is working, still working on timer

--- src/main/java/number/guess/guessnumber/Controler.java
+++ src/main/java/number/guess/guessnumber/Controler.java
@@ -7,26 +7,40 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.util.Duration;
 
 import java.util.Date;
+import java.util.Random;
 
 public class Controler {
 
+    int numberToGuess;
+    int guess;
+    int numOfAttempts = 1;
+    int startTime = 60;
     @FXML
     private Button guessButton;
     @FXML
-    private TextField userGuess;
+    private TextField userGuessField;
     @FXML
     private Label resultDisplay;
     @FXML
     private Label timer;
-    int startTime = 60;
 
     @FXML
     public void initialize() {
-        guessButton.setOnAction(event -> {
-            timer();
+
+        numberToGuess = generateNumber();
+        System.out.println(numberToGuess);
+        timer();
+
+        guessButton.setOnAction(event -> makeGuess());
+
+        userGuessField.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ENTER){
+                makeGuess();
+            }
         });
     }
 
@@ -45,4 +59,49 @@ public class Controler {
         timeline.play();
     }
 
+    private int generateNumber(){
+
+        Random rand = new Random();
+        //System.out.println(number);
+        return rand.nextInt(100)+1;
+    }
+
+
+    private boolean isNumeric(String str){
+
+        if(str == null || str.isEmpty()){
+            return false;
+        }
+        try{
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    private void makeGuess(){
+
+        String input = userGuessField.getText();
+
+        if(isNumeric(input)) {
+
+            guess = Integer.parseInt(input);
+            if (guess == numberToGuess) {
+                resultDisplay.setText("You guessed correctly!\nAttempts: " + numOfAttempts);
+                resultDisplay.getStyleClass().add("correct-guess");
+            } else if (guess > numberToGuess) {
+                resultDisplay.setText("Too high");
+                numOfAttempts++;
+            } else {
+                resultDisplay.setText("Too low");
+                numOfAttempts++;
+            }
+
+        } else if(guess < 0) {
+            resultDisplay.setText("Only numbers from 1 to 100");
+        } else {
+            resultDisplay.setText("Inserted number is not a number");
+        }
+    }
 }
